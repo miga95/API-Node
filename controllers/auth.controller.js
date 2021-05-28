116f4d3949a72f1b54f806de74d4688156f3cd0f
miga <jargal_m@etna-alternance.net>
2021-05-28 15:11:58 +0200

    ok

---
+++ controllers/auth.controller.js
@@ -0,0 +1,15 @@
+const userModel = require('../models/user.model')
+
+
+module.exports.signUp = async (req, res) => {
+    console.log(req.body)
+    const {pseudo, email, password} = req.body
+
+    try{
+        const user = await userModel.create({pseudo, email, password})
+        res.status(201).json({ user: user._id});
+    }
+    catch(err){
+        res.status(200).send({ err })
+    }
+}
