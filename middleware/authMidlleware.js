f44b3a1ac5ad4ca8dd99ac9d7217df66286ee522
miga <jargal_m@etna-alternance.net>
2021-07-12 23:38:45 +0200

    fetch with axios

--- middleware/authMidlleware.js
+++ middleware/authMidlleware.js
@@ -1,5 +1,7 @@
+const cookie =require ('cookie');
 const jwt = require('jsonwebtoken')
 const userModel = require('../models/userModel')
+
 
 module.exports.checkUser =  (req, res, next) =>{
     const token = req.cookies.jwt
@@ -25,11 +27,13 @@ module.exports.checkUser =  (req, res, next) =>{
 module.exports.requireAuth = (req, res, next) => {
     const token = req.cookies.jwt
     if(token){
+        
         jwt.verify(token, process.env.TOKEN_SECRET, async (err, decodedToken) => {
             if(err){
                 console.log(err);
+            
             }else{
-                console.log(decodedToken.id);
+               console.log(decodedToken.id);
                 next()
 
             }
